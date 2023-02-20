ce0486734e2beac71a4cae131332926fcf3c15a2
sharlisbg <sharislizza15@gmail.com>
2023-02-20 10:43:11 -0600

    Cambios

--- IPostFixCalculator.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author Seccion 40
- */
-public interface IPostfixCalculator<T> {
-
-	public int Calculate(java.util.ArrayList<T> postfix_expression) throws Exception;
-}
--- Lista.java
+++
@@ -1,7 +0,0 @@
-public interface Lista<T>{
-
-    boolean isEmpty();
-    void add(T value);
-    void remove(T value);
-
-}
